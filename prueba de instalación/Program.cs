a53590d98a4bc5d408621ba94b1cddfdc68a8a48
Julian Filguiera <julianfilgueira2@gmail.com>
2023-08-24 12:02:11 -0300

    MegaCrackcambio 2

--- prueba de instalación/Program.cs
+++ prueba de instalación/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("holaaa");
+
+Console.WriteLine("gerard es mi coordinador");
+Console.WriteLine("Juli un gran padawan tu eres...");
+
